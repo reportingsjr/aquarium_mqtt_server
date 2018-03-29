fff1a198f49031f187e658524803fe8f945aa43e
Jon Neal <reportingsjr@gmail.com>
2018-03-29 13:00:09 -0400

    Start splitting out the parsing function so I can add a builtin test for it.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,27 @@
 extern crate mqttc;
+extern crate mqtt3;
 extern crate netopt;
-use std::process::exit;
 use std::time::{Duration, SystemTime, UNIX_EPOCH};
+#[allow(unused_imports)]
 use std::sync::Arc;
 use netopt::NetworkOptions;
-use mqttc::{PubSub, Client, ClientOptions, ReconnectMethod, PubOpt};
+#[allow(unused_imports)]
+use mqttc::{PubSub, Client, ClientOptions, ReconnectMethod};
+use mqtt3::Message;
+
+struct Temperature {
+	temperature: f64,
+	timestamp: Duration,
+}
 
 fn main() {
-    let address = "192.168.0.22:883";
+    let address = "192.168.0.22:1883";
     // Connect to broker, send CONNECT then wait CONNACK
     let netopt = NetworkOptions::new();
     let mut opts = ClientOptions::new();
     opts.set_keep_alive(15);
     opts.set_reconnect(ReconnectMethod::ReconnectAfter(Duration::new(5,0)));
-    let mut client = opts.connect(address.as_str(), netopt).unwrap();
+    let mut client = opts.connect(address, netopt).unwrap();
 
     client.subscribe("/aquarium/temperature").unwrap();
     client.await().unwrap();
@@ -25,12 +33,9 @@ fn main() {
             Ok(result) => {
                 match result {
                     Some(message) => {
-                    	let message = String::from_utf8(Arc::try_unwrap(message.payload).unwrap()).expect("invalid utf-8");
-                    	let temperature = message.parse::<f64>().unwrap();
-                    	if(temperature < 0.0) {
-                    		println!("Bad temperature reading: {:?}", temperature);
-                    	} else {
-                    		println!("Good temperature reading:{:?} {:?}", SystemTime::now().duration_since(UNIX_EPOCH).expect("failed to get current time").as_secs(), temperature);
+                    	match parse_temperature(message) {
+                    		Ok(temperature) => println!("Timestamp: {:?}, Temperature: {:?}", temperature.timestamp.as_secs(), temperature.temperature),
+                    		Err(_) => continue
                     	}
                     },
                     None => println!("."),
@@ -39,4 +44,23 @@ fn main() {
             Err(_) => continue
         }
     }
+}
+
+fn parse_temperature(message: Box<Message>) -> Result<Temperature, &'static str> {
+	let raw_message = String::from_utf8(Arc::try_unwrap(message.payload).unwrap()).expect("invalid utf-8");
+	let temperature = raw_message.parse::<f64>().unwrap();
+	
+	let time = match SystemTime::now().duration_since(UNIX_EPOCH) {
+		Ok(current_time) => current_time,
+		Err(_) => return Err("failed to get current time")
+	};
+
+	if temperature < 0.0  {
+		return Err("Temperature was outside of known good range.");
+	}
+
+	Ok(Temperature { 
+		temperature: temperature,
+		timestamp: time,
+	})
 }
